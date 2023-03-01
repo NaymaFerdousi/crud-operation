aea372343bce589a8a92f64a859534b6116e60d2
NaymaFerdousi <naimaferdousi211@gmail.com>
2023-03-01 12:01:44 +0600

    first commit

---
+++ server/model/model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+const testTables = mongoose.Connection;
+console.log(testTables);
+
+module.exports = Userdb;
